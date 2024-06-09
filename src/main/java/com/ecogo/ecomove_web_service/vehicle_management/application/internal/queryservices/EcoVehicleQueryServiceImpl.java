cc7f0b3eb45b4a143ce6ab545e66c95f7997d3b1
Christopher <79271081+ChrisByBits@users.noreply.github.com>
2024-06-09 06:38:50 -0500

    feat: add ecovehicle query services

---
+++ src/main/java/com/ecogo/ecomove_web_service/vehicle_management/application/internal/queryservices/EcoVehicleQueryServiceImpl.java
@@ -0,0 +1,71 @@
+package com.ecogo.ecomove_web_service.vehicle_management.application.internal.queryservices;
+
+import com.ecogo.ecomove_web_service.vehicle_management.domain.model.aggregates.EcoVehicle;
+import com.ecogo.ecomove_web_service.vehicle_management.domain.model.queries.*;
+import com.ecogo.ecomove_web_service.vehicle_management.domain.services.EcoVehicleQueryService;
+import com.ecogo.ecomove_web_service.vehicle_management.infrastructure.persistence.jpa.EcoVehicleRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class EcoVehicleQueryServiceImpl implements EcoVehicleQueryService {
+
+    public static EcoVehicleRepository ecoVehicleRepository;
+    public EcoVehicleQueryServiceImpl(EcoVehicleRepository ecoVehicleRepository){
+        this.ecoVehicleRepository = ecoVehicleRepository;
+    }
+
+
+    /**
+     * Handles the GetAllEcoVehicleByBatteryLevelGreaterThanQuery query
+     * @param query the GetAllEcoVehicleByBatteryLevelGreaterThanQuery query
+     * @return - a List of EcoVehicle
+     */
+    @Override
+    public List<EcoVehicle> handle(GetAllEcoVehicleByBatteryLevelGreaterThanQuery query){
+        return ecoVehicleRepository.findAllByBatteryLevelGreaterThan(query.batteryLevel());
+    }
+
+    /**
+     * Handles the GetAllEcoVehicleByTypeAndModelQuery query
+     * @param query the GetAllEcoVehicleByTypeAndModelQuery query
+     * @return - a List of EcoVehicle
+     */
+    @Override
+    public List<EcoVehicle> handle(GetAllEcoVehicleByTypeAndModelQuery query){
+        return ecoVehicleRepository.findAllByTypeAndModel(query.type(), query.model());
+    }
+
+    /**
+     * Handles the GetAllEcoVehicleByTypeAndStatusQuery query
+     * @param query the GetAllEcoVehicleByTypeAndStatusQuery query
+     * @return - a List of EcoVehicle
+     */
+    @Override
+    public List<EcoVehicle> handle(GetAllEcoVehicleByTypeAndStatusQuery query){
+        return ecoVehicleRepository.findAllByTypeAndStatus(query.type(), query.status());
+    }
+
+    /**
+     * Handles the GetAllEcoVehicleByTypeQuery query
+     * @param query the GetAllEcoVehicleByTypeQuery query
+     * @return - a List of EcoVehicle
+     */
+    @Override
+    public List<EcoVehicle> handle(GetAllEcoVehicleByTypeQuery query){
+        return ecoVehicleRepository.findAllByType(query.type());
+    }
+
+    /**
+     * Handles the GetEcoVehicleByIdQuery query
+     * @param query the GetEcoVehicleByIdQuery query
+     * @return - an Optional of EcoVehicle
+     */
+    @Override
+    public Optional<EcoVehicle> handle(GetEcoVehicleByIdQuery query){
+        return ecoVehicleRepository.findById(query.id());
+    }
+
+}
